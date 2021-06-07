3a23432cf2b043c13dfd4261190ee4dc4dfd278d
febinfavas <febinfavas414@gmail.com>
2021-06-07 16:02:02 +0530

    Added Source Files

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Basics.Math;
+
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var febin = new Person();
+            febin.firstName = "Febin";
+            febin.lastName = "Favas";
+
+            febin.Introduce();
+
+            Calculator calc = new Calculator();
+            var result = calc.add(5, 6);
+            Console.WriteLine(result);
+
+            var array = new int[3];
+            array[0]=1;
+            array[1]=2;
+ 
+
+            Console.WriteLine(array[0]);
+            Console.WriteLine(array[1]);
+            Console.WriteLine(array[2]);
+        }
+    }
+}
