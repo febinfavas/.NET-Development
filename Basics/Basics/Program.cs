7977c14d9d5849380407c25cf93a3dd6cb1c4191
febinfavas <febinfavas414@gmail.com>
2021-06-08 12:02:05 +0530

    Added Examples

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -4,6 +4,12 @@ using Basics.Math;
 
 namespace Basics
 {
+    public enum ShippingMethod
+    {
+        RegularMail=1,
+        RegisteredMail=2,
+        Express=3
+    }
     class Program
     {
         static void Main(string[] args)
@@ -23,9 +29,49 @@ namespace Basics
             array[1]=2;
  
 
-            Console.WriteLine(array[0]);
+            Console.WriteLine(array[0]); 
             Console.WriteLine(array[1]);
             Console.WriteLine(array[2]);
+
+            var arrayBool = new bool[3];
+            arrayBool[0] = true;
+
+            Console.WriteLine(arrayBool[0]);
+            Console.WriteLine(arrayBool[1]);
+            Console.WriteLine(arrayBool[2]);
+
+            var names = new string[3] {"febin","favas","madathil"};
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
+            var fName = "Muhsina";
+            var lName = "Hameed";
+
+            var fullName = fName + " " + lName;
+
+            var fullName1 = string.Format("{0} {1}", fName, lName);
+
+            Console.WriteLine("My full Name is " +fullName+" and "+fullName1);
+
+            var numbers = new int[4] { 1, 2, 3, 4 };
+            var list = string.Join(",", numbers);
+
+            Console.WriteLine(list);
+
+            var path = "c:\\project\\folder\\febin";
+            var path1 = @"c:\project\folder\febin";
+            var josh = @"Hi febin
+                        Look into the following path
+                        c:\project\folder\febin
+                        c:\project\folder\favas";
+            Console.WriteLine(path);
+            Console.WriteLine(path1);
+            Console.WriteLine(josh);
+
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
         }
     }
 }
