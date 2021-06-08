7b3024d5d16894a40f73076e8d48e211ad2788ac
febinfavas <febinfavas414@gmail.com>
2021-06-08 17:27:54 +0530

    Added Examples

--- Basics4/Basics4/Program.cs
+++ Basics4/Basics4/Program.cs
@@ -10,6 +10,36 @@ namespace Basics4
     {
         static void Main(string[] args)
         {
+            for (var i= 0;i<10;i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("\n\n");
+
+            var number = new int[5] { 10, 9, 8, 7, 6 };
+            foreach(var num in number)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("\n\n");
+
+            int j = 1;
+            while(j<10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+            Console.WriteLine("\n\n");
+
+            int k = 10;
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+            } while (k < 10);
         }
     }
 }
