241e534d10645ca53693c3cf227a827713b52429
febinfavas <febinfavas414@gmail.com>
2021-06-09 18:45:10 +0530

    Added Examples

---
+++ Basics6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullname = "Febin Favas  ";
+
+            //Trim
+            Console.WriteLine("Without Trim: '{0}'", fullname);
+            Console.WriteLine("Trim: '{0}'", fullname.Trim());
+
+            //upper lower
+            Console.WriteLine("upper0}'", fullname.Trim());
+            Console.WriteLine("Trim: '{0}'", fullname.Trim());
+
+        }
+    }
+}
