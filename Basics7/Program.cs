43ecf05d8af4c11dc71d1048a343be17d7df1262
febinfavas <febinfavas414@gmail.com>
2021-06-10 02:39:22 +0530

    Added Examples

---
+++ Basics7/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File
+            /*
+            var path = @"C:\Users\FEBIN FAVAS M\Desktop\aaaaa.txt";
+            File.Create(@"C:\Users\FEBIN FAVAS M\Desktop\file.txt");
+
+            File.Copy(@"C:\Users\FEBIN FAVAS M\Desktop\aaaaa.txt", @"C:\Users\FEBIN FAVAS M\Desktop\Febin\aaaaa.txt", true);
+
+            File.Delete(path);
+
+            var content = File.ReadAllText(path);
+            Console.WriteLine("content in files are :"+content);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.AppendText();
+            fileInfo.Delete();
+            */
+
+            //Directory
+            /*
+            var path = @"C:\Users\FEBIN FAVAS M\Desktop\f1";
+            Directory.CreateDirectory(path);
+
+            var files = Directory.GetFiles(@"C:\Users\FEBIN FAVAS M\Desktop","*.*",SearchOption.AllDirectories);
+            foreach(var i in files)
+                Console.WriteLine(i);
+
+            var directoryInfo = new DirectoryInfo(@"C:\Users\FEBIN FAVAS M\Desktop");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+            */
+
+
+            //Path
+            /*
+            var path = @"C:\Users\FEBIN FAVAS M\Desktop";
+            Console.WriteLine(path.IndexOf('M'));
+            Console.WriteLine(Path.GetDirectoryName(path));
+            
+
+            */
+        }
+    }
+}
